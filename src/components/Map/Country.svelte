55eb07ee0449130d5a0efd597a38fc1849a476bc
Lauren Kim <laurenkim0424@gmail.com>
2026-08-24 13:20:36 -0400

    Reduce fallback line width for regions

--- src/components/Map/Country.svelte
+++ src/components/Map/Country.svelte
@@ -32,7 +32,7 @@
 
   if (mode === 'stroke') {
     ctx.strokeStyle = fallbackFillColor;
-    ctx.lineWidth = 3.5;  // <--- thinner fallback for regions
+    ctx.lineWidth = 2.5;  // <--- thinner fallback for regions
     ctx.stroke(p);
   }
 }
